a8606ed1fc09c5eb5226cc5f078f63f7b5714893
Felix Arntz <flixos90@gmail.com>
2024-07-24 19:18:25 -0700

    Use plugin text domain for all library translation strings.

--- scoper.inc.php
+++ scoper.inc.php
@@ -8,6 +8,13 @@
 use Symfony\Component\Finder\Finder;
 
 /*
+ * Specify the text domain for this plugin.
+ * It will be used in all translation strings from the underlying library, to make sure they are properly translatable
+ * as part of the plugin.
+ */
+$plugin_textdomain = 'wp-oop-plugin-lib-example';
+
+/*
  * You can optionally provide specific folder names from the WP OOP Plugin Lib "src" directory here to limit the
  * library classes in your project to only those. If you only need a few classes from the library in your project, this
  * helps keep the PHP footprint and the project's file size small.
@@ -38,6 +45,25 @@ return array(
 			->exclude( array( 'docs', 'tests' ) )
 			->path( $wp_oop_plugin_lib_folders_regex )
 			->in( 'vendor/felixarntz/wp-oop-plugin-lib/src' ),
+	),
+	'patchers'        => array(
+		// Patcher to replace the library text domain with the plugin text domain.
+		function ( $file_path, $prefix, $content ) use ( $plugin_textdomain ) {
+			if ( $plugin_textdomain ) {
+				return str_replace(
+					array(
+						"'wp-oop-plugin-lib' )",
+						"'wp-oop-plugin-lib')",
+					),
+					array(
+						"'" . $plugin_textdomain . "' )",
+						"'" . $plugin_textdomain . "')",
+					),
+					$content
+				);
+			}
+			return $content;
+		}
 	),
 	'exclude-classes' => array( '/^WP_[A-Za-z0-9_]+$/' ),
 );

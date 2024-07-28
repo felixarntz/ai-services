e74755206aee8fa3940d4e048680758bfd4f1bc4
Felix Arntz <flixos90@gmail.com>
2024-07-28 14:38:50 -0700

    Fix failing test.

--- tests/phpunit/tests/Plugin_Main_Tests.php
+++ tests/phpunit/tests/Plugin_Main_Tests.php
@@ -8,8 +8,10 @@
 
 namespace Vendor_NS\WP_OOP_Plugin_Lib_Example\PHPUnit\Tests;
 
+use ReflectionProperty;
 use Vendor_NS\WP_OOP_Plugin_Lib_Example\PHPUnit\Includes\Test_Case;
 use Vendor_NS\WP_OOP_Plugin_Lib_Example\Plugin_Main;
+use Vendor_NS\WP_OOP_Plugin_Lib_Example\Services\Services_API_Instance;
 
 class Plugin_Main_Tests extends Test_Case {
 
@@ -17,13 +19,24 @@ class Plugin_Main_Tests extends Test_Case {
 
 	public function set_up() {
 		parent::set_up();
+
+		// Clear up the singleton-like instance to prevent error from trying to set it again by via Plugin_Main.
+		$prop = new ReflectionProperty( Services_API_Instance::class, 'instance' );
+		$prop->setAccessible( true );
+		$prop->setValue( null, null );
+		$prop->setAccessible( false );
+
 		$this->plugin_main = new Plugin_Main( TESTS_PLUGIN_DIR . '/load.php' );
 	}
 
 	public function test_add_hooks() {
 		$actions_to_check = array(
+			// Via Plugin_Main.
 			'init',
 			'activate_' . basename( TESTS_PLUGIN_DIR ) . '/load.php',
+
+			// Via Services_Loader.
+			'plugins_loaded',
 			'admin_menu',
 		);
 		foreach ( $actions_to_check as $hook_name ) {

d7a8f76f7ad78e6ddb1ca8052102fba00d9bfde0
Felix Arntz <flixos90@gmail.com>
2024-10-09 16:50:14 -0700

    Implement example plugin for editor command to have AI write the post excerpt.

---
+++ examples/write-excerpt-plugin/index.js
@@ -0,0 +1,96 @@
+/*
+ * Props to https://github.com/swissspidy, since most of this code is copied or at least heavily inspired from his code
+ * in https://github.com/swissspidy/ai-experiments/blob/main/packages/editor/src/commands.tsx.
+ */
+
+const { useCommandLoader } = wp.commands;
+const { Icon } = wp.components;
+const { useDispatch, useSelect } = wp.data;
+const { createElement, useState } = wp.element;
+const { store: blockEditorStore } = wp.blockEditor;
+const { store: editorStore } = wp.editor;
+const { store: aiStore } = window.aiServices.aiStore;
+const { serialize } = wp.blocks;
+const { registerPlugin } = wp.plugins;
+const { __ } = wp.i18n;
+
+const AI_CAPABILITIES = [ 'text_generation' ];
+
+function useWriteExcerptCommandLoader() {
+	const { editPost } = useDispatch( editorStore );
+
+	const [ isLoading ] = useState( false );
+
+	const { postContent } = useSelect( ( select ) => {
+		const content =
+			new window.DOMParser().parseFromString(
+				serialize( select( blockEditorStore ).getBlocks() ),
+				'text/html'
+			).body.textContent || '';
+		return {
+			postContent: content,
+		};
+	}, [] );
+
+	const service = useSelect( ( select ) =>
+		select( aiStore ).getAvailableService( AI_CAPABILITIES )
+	);
+	if ( ! service ) {
+		return {
+			commands: [],
+			isLoading: true,
+		};
+	}
+
+	const commands = [
+		{
+			name: 'write-excerpt-plugin/write-excerpt',
+			label: __( 'Write excerpt', 'write-excerpt-plugin' ),
+			icon: createElement( Icon, { icon: 'edit' } ),
+			// @ts-ignore
+			callback: async ( { close } ) => {
+				close();
+
+				let candidates;
+				try {
+					candidates = await service.generateText(
+						__(
+							'Summarize the following text in full sentences in less than 300 characters:',
+							'write-excerpt-plugin'
+						) +
+							' ' +
+							postContent,
+						{ feature: 'write-excerpt-plugin' }
+					);
+				} catch ( error ) {
+					window.console.error( error );
+					return;
+				}
+
+				const excerpt =
+					candidates[ 0 ].content.parts[ 0 ].text.replaceAll(
+						'\n\n\n\n',
+						'\n\n'
+					);
+				editPost( { excerpt } );
+			},
+		},
+	];
+
+	return {
+		commands,
+		isLoading,
+	};
+}
+
+function RenderPlugin() {
+	useCommandLoader( {
+		name: 'write-excerpt-plugin/commands',
+		hook: useWriteExcerptCommandLoader,
+	} );
+	return null;
+}
+
+registerPlugin( 'write-excerpt-plugin', {
+	render: RenderPlugin,
+} );
---
+++ examples/write-excerpt-plugin/write-excerpt-plugin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name: Write Excerpt Plugin
+ * Plugin URI: https://wordpress.org/plugins/ai-services/
+ * Description: Example plugin using AI Services to implement a command to write a post excerpt.
+ * Requires at least: 6.0
+ * Requires PHP: 7.2
+ * Version: 1.0.0
+ * Author: Felix Arntz
+ * Author URI: https://felix-arntz.me
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: write-excerpt-plugin
+ * Requires Plugins: ai-services
+ */
+
+add_action(
+	'enqueue_block_editor_assets',
+	static function () {
+		if ( ! function_exists( 'ai_services' ) ) {
+			return;
+		}
+
+		wp_enqueue_script(
+			'write-excerpt-plugin',
+			plugin_dir_url( __FILE__ ) . 'index.js',
+			array(
+				'wp-commands',
+				'wp-components',
+				'wp-data',
+				'wp-element',
+				'wp-block-editor',
+				'wp-editor',
+				'ais-ai-store',
+				'wp-blocks',
+				'wp-plugins',
+				'wp-i18n',
+			),
+			'1.0.0',
+			array( 'strategy' => 'defer' )
+		);
+	}
+);

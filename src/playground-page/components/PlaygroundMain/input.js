f44efa501bde7a0ef8ad104b4c7ae54fe523d148
Felix Arntz <flixos90@gmail.com>
2024-12-23 19:44:45 +0100

    Enhance playground input by allowing arrow navigation to access previous messages and automatically focusing on it.

--- src/playground-page/components/PlaygroundMain/input.js
+++ src/playground-page/components/PlaygroundMain/input.js
@@ -7,9 +7,10 @@ import { enums, store as aiStore } from '@ai-services/ai';
  * WordPress dependencies
  */
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useState } from '@wordpress/element';
+import { useCallback, useEffect, useState, useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { send, upload } from '@wordpress/icons';
+import { UP, DOWN } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
@@ -17,6 +18,60 @@ import { send, upload } from '@wordpress/icons';
 import { store as playgroundStore } from '../../store';
 
 const EMPTY_ARRAY = [];
+
+const matchMessage = ( message, prompt ) => {
+	if ( message.type !== 'user' ) {
+		return '';
+	}
+
+	if ( prompt === '' && message.content.parts[ 0 ]?.text ) {
+		return message.content.parts[ 0 ].text;
+	}
+
+	for ( let j = 0; j < message.content.parts.length; j++ ) {
+		if (
+			message.content.parts[ j ].text &&
+			message.content.parts[ j ].text.startsWith( prompt ) &&
+			message.content.parts[ j ].text !== prompt
+		) {
+			return message.content.parts[ j ].text;
+		}
+	}
+
+	return '';
+};
+
+const matchLastMessage = (
+	messages,
+	prompt,
+	matchedIndex = -1,
+	searchForwards = false
+) => {
+	if ( ! messages || ! messages.length ) {
+		return [ -1, '' ];
+	}
+
+	if ( searchForwards ) {
+		const startIndex = matchedIndex === -1 ? 0 : matchedIndex + 1;
+		for ( let i = startIndex; i < messages.length; i++ ) {
+			const match = matchMessage( messages[ i ], prompt );
+			if ( match ) {
+				return [ i, match ];
+			}
+		}
+	} else {
+		const startIndex =
+			matchedIndex === -1 ? messages.length - 1 : matchedIndex - 1;
+		for ( let i = startIndex; i >= 0; i-- ) {
+			const match = matchMessage( messages[ i ], prompt );
+			if ( match ) {
+				return [ i, match ];
+			}
+		}
+	}
+
+	return [ -1, '' ];
+};
 
 /**
  * Renders the prompt input UI.
@@ -27,33 +82,41 @@ const EMPTY_ARRAY = [];
  */
 export default function Input() {
 	const [ prompt, setPrompt ] = useState( '' );
+	const [ promptToMatch, setPromptToMatch ] = useState( false );
+	const [ matchedIndex, setMatchedIndex ] = useState( -1 );
 
-	const { service, model, capabilities } = useSelect( ( select ) => {
-		const { getService, getModel } = select( playgroundStore );
-		const { getServices } = select( aiStore );
+	const { service, model, capabilities, messages } = useSelect(
+		( select ) => {
+			const { getService, getModel, getMessages } =
+				select( playgroundStore );
+			const { getServices } = select( aiStore );
 
-		const currentService = getService();
-		const currentModel = getModel();
+			const currentService = getService();
+			const currentModel = getModel();
 
-		let currentCapabilities = EMPTY_ARRAY;
-		if ( currentService && currentModel ) {
-			const services = getServices();
-			if (
-				services &&
-				services[ currentService ] &&
-				services[ currentService ].available_models[ currentModel ]
-			) {
-				currentCapabilities =
-					services[ currentService ].available_models[ currentModel ];
+			let currentCapabilities = EMPTY_ARRAY;
+			if ( currentService && currentModel ) {
+				const services = getServices();
+				if (
+					services &&
+					services[ currentService ] &&
+					services[ currentService ].available_models[ currentModel ]
+				) {
+					currentCapabilities =
+						services[ currentService ].available_models[
+							currentModel
+						];
+				}
 			}
-		}
 
-		return {
-			service: currentService,
-			model: currentModel,
-			capabilities: currentCapabilities,
-		};
-	} );
+			return {
+				service: currentService,
+				model: currentModel,
+				capabilities: currentCapabilities,
+				messages: getMessages(),
+			};
+		}
+	);
 
 	const { sendMessage } = useDispatch( playgroundStore );
 
@@ -64,15 +127,61 @@ export default function Input() {
 			return;
 		}
 
-		await sendMessage( prompt );
+		setPromptToMatch( false );
+		setMatchedIndex( -1 );
 		setPrompt( '' );
+		await sendMessage( prompt );
 	};
+
+	const searchLastMessage = useCallback(
+		( event ) => {
+			if ( event.keyCode === UP || event.keyCode === DOWN ) {
+				if ( false === promptToMatch ) {
+					setPromptToMatch( prompt );
+				}
+				const [ foundIndex, matchedMessage ] = matchLastMessage(
+					messages,
+					false === promptToMatch ? prompt : promptToMatch,
+					matchedIndex,
+					event.keyCode === DOWN
+				);
+				if ( matchedMessage ) {
+					setPrompt( matchedMessage );
+					setMatchedIndex( foundIndex );
+				}
+			}
+		},
+		[ messages, prompt, promptToMatch, matchedIndex ]
+	);
+
+	const inputRef = useRef();
+
+	useEffect( () => {
+		if ( ! inputRef.current ) {
+			return;
+		}
+
+		inputRef.current.focus();
+	}, [ inputRef ] );
+
+	useEffect( () => {
+		if ( ! inputRef.current ) {
+			return;
+		}
+
+		const inputElement = inputRef.current;
+		inputElement.addEventListener( 'keydown', searchLastMessage );
+		return () => {
+			inputElement.removeEventListener( 'keydown', searchLastMessage );
+		};
+	}, [ inputRef, searchLastMessage ] );
 
 	return (
 		<div className="ai-services-playground__input-backdrop">
 			<div className="ai-services-playground__input-container">
 				<textarea
 					className="ai-services-playground__input"
+					ref={ inputRef }
 					placeholder={ __( 'Enter AI prompt', 'ai-services' ) }
 					aria-label={ __( 'AI prompt', 'ai-services' ) }
 					value={ prompt }

34b85ecbc42d0dac8964b313e1922d193d5559a7
Felix Arntz <flixos90@gmail.com>
2025-01-28 17:31:31 -0800

    Update NPM production dependencies and fix deprecation warning.

--- src/settings-page/components/SettingsCards/index.js
+++ src/settings-page/components/SettingsCards/index.js
@@ -62,6 +62,7 @@ export default function SettingsCards() {
 						disabled={ isLoading }
 						checked={ deleteData || false }
 						onChange={ setDeleteData }
+						__nextHasNoMarginBottom
 					/>
 				</CardBody>
 			</Card>

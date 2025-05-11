d47a80d4deeed121f5f532f3b478d142919f6166
Felix Arntz <flixos90@gmail.com>
2025-05-11 14:14:46 -0700

    Implement import resolver to properly solve missing import lint errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,41 +3,62 @@
  */
 const config = require( '@wordpress/scripts/config/.eslintrc' );
 
+const extraSettings = {
+	'import/resolver': require.resolve( './tools/js/eslint-import-resolver' ),
+};
+
+const extraRules = {
+	// Require JS docblocks for all functions, just like in PHP.
+	'jsdoc/require-jsdoc': [
+		'error',
+		{
+			require: {
+				FunctionDeclaration: true,
+				MethodDefinition: true,
+				ClassDeclaration: true,
+				ArrowFunctionExpression: false,
+				FunctionExpression: true,
+			},
+		},
+	],
+	'jsdoc/require-description': 'error',
+	'jsdoc/require-param': 'error',
+	'jsdoc/require-param-description': 'error',
+	'jsdoc/require-param-name': 'error',
+	'jsdoc/require-param-type': 'error',
+	'jsdoc/require-returns-check': 'error',
+	'jsdoc/require-returns-description': 'error',
+	'jsdoc/require-returns-type': 'error',
+	'jsdoc/require-returns': 'error',
+};
+
+const typescriptRules = {
+	'jsdoc/require-param-type': 'off',
+	'jsdoc/require-returns-type': 'off',
+	'no-unused-vars': 'off',
+	'@typescript-eslint/no-unused-vars': [
+		'error',
+		{ ignoreRestSiblings: true },
+	],
+	'no-shadow': 'off',
+	'@typescript-eslint/no-shadow': 'error',
+	'@typescript-eslint/method-signature-style': 'error',
+	'tsdoc/syntax': 'error',
+};
+
 module.exports = {
 	...config,
 	extends: [
 		...( config.extends || [] ),
 		'plugin:@wordpress/eslint-plugin/i18n',
 	],
+	settings: {
+		...( config.settings || {} ),
+		...extraSettings,
+	},
 	rules: {
-		...config.rules,
-		'import/no-unresolved': [
-			'error',
-			{ ignore: [ '^@wordpress/', '^@wp-starter-plugin/' ] },
-		],
-
-		// Require JS docblocks for all functions, just like in PHP.
-		'jsdoc/require-jsdoc': [
-			'error',
-			{
-				require: {
-					FunctionDeclaration: true,
-					MethodDefinition: true,
-					ClassDeclaration: true,
-					ArrowFunctionExpression: false,
-					FunctionExpression: true,
-				},
-			},
-		],
-		'jsdoc/require-description': 'error',
-		'jsdoc/require-param': 'error',
-		'jsdoc/require-param-description': 'error',
-		'jsdoc/require-param-name': 'error',
-		'jsdoc/require-param-type': 'error',
-		'jsdoc/require-returns-check': 'error',
-		'jsdoc/require-returns-description': 'error',
-		'jsdoc/require-returns-type': 'error',
-		'jsdoc/require-returns': 'error',
+		...( config.rules || [] ),
+		...extraRules,
 	},
 	overrides: [
 		...( config.overrides || [] ),
@@ -58,7 +79,10 @@ module.exports = {
 				tsconfigRootDir: __dirname,
 			},
 			settings: {
+				...( config.settings || {} ),
+				...extraSettings,
 				jsdoc: {
+					mode: 'typescript',
 					// TSDoc expects `@returns` and `@yields`.
 					tagNamePreference: {
 						returns: 'returns',
@@ -67,19 +91,9 @@ module.exports = {
 				},
 			},
 			rules: {
-				'no-duplicate-imports': 'off',
-				'import/no-duplicates': 'error',
-				'jsdoc/require-param-type': 'off',
-				'jsdoc/require-returns-type': 'off',
-				'no-unused-vars': 'off',
-				'@typescript-eslint/no-unused-vars': [
-					'error',
-					{ ignoreRestSiblings: true },
-				],
-				'no-shadow': 'off',
-				'@typescript-eslint/no-shadow': 'error',
-				'@typescript-eslint/method-signature-style': 'error',
-				'tsdoc/syntax': 'error',
+				...( config.rules || [] ),
+				...extraRules,
+				...typescriptRules,
 			},
 		},
 	],
